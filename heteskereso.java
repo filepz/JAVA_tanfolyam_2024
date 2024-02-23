c1ac9512f2cde568475a19a542daf9b0f5e90cb4
Zoltan Filep <92667297+filepz@users.noreply.github.com>
2024-02-23 21:50:53 +0100

    Update heteskereso.java

--- heteskereso.java
+++ heteskereso.java
@@ -1,26 +1,97 @@
-def heteskereso():
-   szoveg = input("Adjon meg egy egesz szamot!")
-   szam = int(szoveg)
-   i=1
-   vanhetes=False
-   while szam>=7:
-       osztasutan=szam//10
-       maradek=szam%10
-       if maradek==7:
-          vanhetes=True
-          break
-       szam=osztasutan
-   print(vanhetes)
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //heteskereso();
+        //ketszam();
+        //teszt();
+        //elsoezer();
+        tokeletes();
+    }
+
+    public static void heteskereso() {
+        boolean vanhetes = false;
+        System.out.println("Keressük meg a megadott pozitiv egész  számban a '7'-es számjegyet.");
+        System.out.print("Adja meg a számot?");
+
+        Scanner sc = new Scanner(System.in);
+        int szam = sc.nextInt();
+        while (szam >= 7) {
+            int osztasutan = szam / 10;
+            int maradek = szam % 10;
+            if (maradek == 7) {
+                vanhetes = true;
+                break;
+            }
+            szam = osztasutan;
+
+        }
+        System.out.print(vanhetes);
+    }
+    public static boolean primszam(int szam) {
+        boolean primszam2 = true;
+        for (int i = 2; i <= szam/2; i++) {
+            if (szam % i == 0) {
+                primszam2=false;
+                //return  primszam2;
+            }
+        }
+        return primszam2;
+    }
+    public static void ketszam(){
+        int also;
+        int felso;
+        int szam1;
+        int szam2;
+        System.out.print("add meg az elso szamot");
+        Scanner sc = new Scanner(System.in);
+        szam1=sc.nextInt();
+        System.out.print("add meg a masodik szamot");
+        Scanner sc2 = new Scanner(System.in);
+        szam2=sc2.nextInt();
+        if (szam1<=szam2) {
+            also=szam1;
+            felso=szam2;
+        }else {
+            also=szam2;
+            felso=szam1;
+        }
+        System.out.println("also"+also+"felso"+felso);
+        for (int i=also;i<=felso;i++){
+            if (primszam(i)){
+                System.out.println(i);
+            }
+        }
 
 
-def primszamok():
-    print("Két szám közötti primszámokat keressük")
-    szoveg1 = input("Adjon meg az egyik egesz szamot!")
-    szam1 = int(szoveg1)
-    szoveg2 = input("Adjon meg a masik egesz szamot!")
-    szam2 = int(szoveg2)
-
-
-if __name__ == '__main__':
-   # heteskereso()
-   primszamok()
+    }
+    public static void teszt(){
+        System.out.print("add meg a szamot");
+        Scanner sc = new Scanner(System.in);
+        System.out.print(primszam(sc.nextInt()));
+    }
+    public static void elsoezer(){
+        int i=0;
+        int k=2;
+        while (i<=1000){
+            if (primszam(k)){
+                i=i+1;
+            }
+            k++;
+        }
+        System.out.println(k-1);
+    }
+    public static void tokeletes(){
+        int osszeg=0;
+        System.out.print("Adja meg a számot");
+        Scanner sc = new Scanner(System.in);
+        int szam=sc.nextInt();
+        for(int j=1;j<szam;j++){
+            if (szam%j==0){
+                System.out.println("oszto:"+j);
+                osszeg=osszeg+j;
+            }
+        }
+        System.out.println(osszeg);
+    }
+}
